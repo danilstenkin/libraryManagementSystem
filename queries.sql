6fe1dfd3dda241bda79554d459e9d907039e40c6
rkaipzha <student@ALEM-F4-L02-01.alem.school>
2025-03-02 00:22:35 +0500

    chore: change table name from  uppercase to lowercase

--- queries.sql
+++ queries.sql
@@ -57,8 +57,8 @@ SELECT
     a.first_name || ' ' || a.last_name AS author_name,
     AVG(2025 - b.year_published) AS average_book_age
 FROM
-    AUTHORS a
-    JOIN BOOKS b ON a.author_id = b.author_id
+    authors a
+    JOIN books b ON a.author_id = b.author_id
 GROUP BY
     a.author_id
 ORDER BY
