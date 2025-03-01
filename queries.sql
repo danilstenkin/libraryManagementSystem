ee64aaae9b66f947cb65979aea2cdd14b08fd833
rkaipzha <student@ALEM-F4-L02-01.alem.school>
2025-03-02 00:09:49 +0500

    feat: Correlated Subqueries:

--- queries.sql
+++ queries.sql
@@ -28,3 +28,25 @@ HAVING
             1
     );
 
+--- 12. Single Value Subqueries
+SELECT
+    p.patron_id,
+    (p.first_name || ' ' || p.last_name) AS full_name,
+    g.genre_name
+FROM
+    patrons p
+    JOIN loans l ON p.patron_id = l.patron_id
+    JOIN books b ON l.book_id = b.book_id
+    JOIN genres g ON b.genre_id = g.genre_id
+GROUP BY
+    p.patron_id,
+    g.genre_id
+HAVING
+    count(DISTINCT l.book_id) = (
+        SELECT
+            count(*)
+        FROM
+            books b2
+        WHERE
+            b2.genre_id = g.genre_id
+    );
